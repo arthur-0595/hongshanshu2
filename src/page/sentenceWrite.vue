67cb894327fd37a397d9e891ea58b7b3fbe729a5
尹彦 <751410306@qq.com>
2018-02-05 18:14:48 +0800

    静态页链接基础完成

---
+++ src/page/sentenceWrite.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        $END$
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sentence-write',
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
