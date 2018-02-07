3a1ac8e040fe0c193ad00cdeb21e9aaebb00a26d
尹彦 <751410306@qq.com>
2018-02-07 18:22:42 +0800

    s

---
+++ src/components/audioTag.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        $END$
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'audio-tag',
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
