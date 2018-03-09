8e9dfaf7847c4272a89ba669af0f965c9f165fc7
尹彦 <751410306@qq.com>
2018-03-09 18:41:54 +0800

    ss

---
+++ src/page/intelligentVoiceLetterTest.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="wordTest">
+    <testHead :title="test_type"></testHead>
+
+    <home-foot></home-foot>
+    <audio src="" id="audio" autoplay="autoplay"></audio>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'intelligent-voice-word-test',
+    components: {},
+    data() {
+      return {
+
+      }
+    },
+    methods: {},
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    created() {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/page/intelligentVoiceLetterTestScore.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'intelligent-voice-word-test-score',
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
